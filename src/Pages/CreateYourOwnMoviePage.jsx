438bc49283cf7e1aaa93ec70477802d1b6a7d046
“Nicole <itsnicoleduffy@gmail.com>
2023-10-12 21:07:04 +0200

    stars didnt work

--- src/Pages/CreateYourOwnMoviePage.jsx
+++ src/Pages/CreateYourOwnMoviePage.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from 'react-router-dom';
-import StarsVideo from "../assets/STARS.mp4";
+import StarsVideo from "../assets/Stars.mp4";
+
 
 function CreateYourOwnMoviePage() {
   const navigate = useNavigate();
@@ -63,155 +64,45 @@ function CreateYourOwnMoviePage() {
   }
 
   return (
-    <div className="createYourOwnMoviePageCompleteScreen">
-      <video
-        autoPlay
-        loop
-        muted
-        style={{
-          position: "fixed",
-          width: "100%",
-          height: "100%",
-          zIndex: -1,
-        }}
-      >
-        <source src={StarsVideo} type="video/mp4" />
-      </video>
 
-      <form className="createMovieForm" onSubmit={onSubmit}>
-        <table>
-          <tr>
-            <td><label for="title">Title:</label></td>
-            <td><input value={title} onChange={event => setTitle(event.target.value)} required id="title" /></td>
-            <td><label for="year">Year:</label></td>
-            <td><input type='text' value={year} onChange={event => setYear(event.target.value)} required inputMode="numeric" pattern="[0-9]*" /></td>
-            <td><label for="rating">Rating:</label></td>
-            <td><input type='text' value={rating} onChange={event => setRating(event.target.value)} required inputMode="decimal" pattern="[0-9]*" /></td>
-            <td><label for="description">Description:</label></td>
-            <td><input value={description} onChange={event => setDescription(event.target.value)} required /></td>
-            <td><label for="Image">Image:</label></td>
-            <td><input value={imageUrl} onChange={event => setImageUrl(event.target.value)} required /></td>
-            <td><label for="review">Review:</label></td>
-            <td><input value={review} onChange={event => setReview(event.target.value)} required /></td>
-          </tr>
-        </table>
-        <button type='submit'>Create New Movie</button>
-      </form>
+    <div className="createYourOwnMoviePageCompleteScreen">
+      <form className="createMovieForm"
+      onSubmit={onSubmit} >
+      <table>
+        <tr>
+          <td><label for="title">Title:</label>
+          </td>
+          <td><input value={title} onChange={event => setTitle(event.target.value)} required id="title" />
+          </td>
+          <td><label for="year">Year:</label>
+          </td>
+          <td><input type='text' value={year} onChange={event => setYear(event.target.value)} required inputMode="numeric" pattern="[0-9]*" />
+          </td>
+          <td><label for="rating">Rating:</label>
+          </td>
+          <td><input type='text' value={rating} onChange={event => setRating(event.target.value)} required inputMode="decimal" pattern="[0-9]*" />
+          </td>
+          <td><label for="description">Description:</label>
+          </td>
+          <td><input value={description} onChange={event => setDescription(event.target.value)} required />
+          </td>
+          <td><label for="Image">Image:</label>
+          </td>
+          <td><input value={imageUrl} onChange={event => setImageUrl(event.target.value)} required />
+          </td>
+          <td><label for="review">Review:</label>
+          </td>
+          <td><input value={review} onChange={event => setReview(event.target.value)} required />
+          </td>
+        </tr>
+      </table>
+      <button type='submit'>Create New Movie</button>
+    </form>
     </div>
+
   );
 }
 
 export default CreateYourOwnMoviePage;
-
-
-
-
-
-// import { useState } from "react";
-// import { useNavigate } from 'react-router-dom';
-// import StarsVideo from "../assets/Stars.mp4";
-
-
-// function CreateYourOwnMoviePage() {
-//   const navigate = useNavigate();
-//   const [title, setTitle] = useState("");
-//   const [year, setYear] = useState("");
-//   const [rating, setRating] = useState("");
-//   const [imageUrl, setImageUrl] = useState("");
-//   const [review, setReview] = useState("");
-//   const [description, setDescription] = useState("");
-//   const [error, setError] = useState("");
-
-//   const onSubmit = async (event) => {
-//     event.preventDefault();
-//     setError("");
-
-//     const values = {
-//       title,
-//       year,
-//       rating,
-//       imageUrl,
-//       review,
-//       description,
-//     };
-
-//     if (!values.title.trim() || !values.year.trim() || !values.rating.trim()) {
-//       setError("All fields are required.");
-//       return;
-//     }
-
-//     const yearNumber = parseInt(values.year, 10);
-//     if (isNaN(yearNumber) || yearNumber < 1900) {
-//       setError("Invalid year.");
-//       return;
-//     }
-
-//     const ratingNumber = parseFloat(values.rating);
-//     if (isNaN(ratingNumber) || ratingNumber < 1 || ratingNumber > 5) {
-//       setError("Rating must be between 1 and 5.");
-//       return;
-//     }
-
-//     try {
-//       const response = await fetch(`${import.meta.env.VITE_API_URL}/Movies`, {
-//         method: 'POST',
-//         body: JSON.stringify(values),
-//         headers: { 'Content-type': 'application/json' },
-//       });
-
-//       if (response.ok) {
-//         const newMovie = await response.json();
-//         console.log(newMovie);
-//         navigate(`/MoviesDetailsPage/${newMovie.id}`);
-//       } else {
-//         setError("An error occurred while creating the movie.");
-//       }
-//     } catch (error) {
-//       console.error(error);
-//       setError("An error occurred while creating the movie.");
-//     }
-//   }
-
-//   return (
-
-//     <div className="createYourOwnMoviePageCompleteScreen">
-//       <form className="createMovieForm"
-//       onSubmit={onSubmit} >
-//       <table>
-//         <tr>
-//           <td><label for="title">Title:</label>
-//           </td>
-//           <td><input value={title} onChange={event => setTitle(event.target.value)} required id="title" />
-//           </td>
-//           <td><label for="year">Year:</label>
-//           </td>
-//           <td><input type='text' value={year} onChange={event => setYear(event.target.value)} required inputMode="numeric" pattern="[0-9]*" />
-//           </td>
-//           <td><label for="rating">Rating:</label>
-//           </td>
-//           <td><input type='text' value={rating} onChange={event => setRating(event.target.value)} required inputMode="decimal" pattern="[0-9]*" />
-//           </td>
-//           <td><label for="description">Description:</label>
-//           </td>
-//           <td><input value={description} onChange={event => setDescription(event.target.value)} required />
-//           </td>
-//           <td><label for="Image">Image:</label>
-//           </td>
-//           <td><input value={imageUrl} onChange={event => setImageUrl(event.target.value)} required />
-//           </td>
-//           <td><label for="review">Review:</label>
-//           </td>
-//           <td><input value={review} onChange={event => setReview(event.target.value)} required />
-//           </td>
-//         </tr>
-//       </table>
-//       <button type='submit'>Create New Movie</button>
-//     </form>
-//     </div>
-
-//   );
-// }
-
-// export default CreateYourOwnMoviePage;
 
 
