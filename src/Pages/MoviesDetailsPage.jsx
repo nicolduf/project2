9d4987dd116816de37c36babf6c147d382773f9a
“Nicole <itsnicoleduffy@gmail.com>
2023-10-13 09:32:52 +0200

    edit button

--- src/Pages/MoviesDetailsPage.jsx
+++ src/Pages/MoviesDetailsPage.jsx
@@ -4,8 +4,33 @@ import { useParams, useNavigate } from "react-router-dom";
 function MoviesDetailsPage() {
     const { movieId } = useParams();
     const [movie, setMovie] = useState(null);
+    const [isEditing, setIsEditing] = useState(false); // Toggle edit mode
     const apiUrl = `${import.meta.env.VITE_API_URL}/Movies/${movieId}`;
-    const navigate = useNavigate(); // Initialize useNavigate from react-router-dom
+    const navigate = useNavigate();
+
+    const editMovie = () => {
+        setIsEditing(true);
+    };
+
+    const updateMovie = (e) => {
+        e.preventDefault();
+        fetch(apiUrl, {
+            method: "PUT",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(movie),
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                setIsEditing(false);
+            })
+            .catch((error) => {
+                console.error("Error updating movie:", error);
+            });
+    };
 
     const deleteMovie = () => {
         fetch(apiUrl, {
@@ -15,13 +40,16 @@ function MoviesDetailsPage() {
                 if (!response.ok) {
                     throw new Error("Network response was not ok");
                 }
+                return response.json(); // Parse the response to JSON
+            })
+            .then(() => {
                 setMovie(null);
                 navigate('/AllMoviesPage');
             })
             .catch((error) => {
                 console.error("Error deleting movie:", error);
             });
-    }
+    };
 
     useEffect(() => {
         fetch(apiUrl)
@@ -45,18 +73,32 @@ function MoviesDetailsPage() {
 
     return (
         <div className="movie-details">
-            <h1>{movie.title}</h1>
-            <p> {movie.year}</p>
-            <p> {movie.rating} Stars</p>
-            <p> {movie.description}</p>
-            <img src={movie.imageUrl} alt={movie.title} className="moviesDetailsPageImages" />
-            <br></br>
-            <button onClick={deleteMovie}>Delete Movie</button>
+            {isEditing ? (
+                <form onSubmit={updateMovie}>
+                    <label>Title: </label>
+                    <input type="text" value={movie.title} onChange={(e) => setMovie({ ...movie, title: e.target.value })} />
+                    <label>Year: </label>
+                    <input type="number" value={movie.year} onChange={(e) => setMovie({ ...movie, year: e.target.value })} />
+                    <label>Rating: </label>
+                    <input type="number" value={movie.rating} onChange={(e) => setMovie({ ...movie, rating: e.target.value })} />
+                    <label>Description: </label>
+                    <textarea value={movie.description} onChange={(e) => setMovie({ ...movie, description: e.target.value })} />
+                    <button type="submit">Update Movie</button>
+                </form>
+            ) : (
+                <>
+                    <h1>{movie.title}</h1>
+                    <p>{movie.year}</p>
+                    <p>{movie.rating} Stars</p>
+                    <p>{movie.description}</p>
+                    <img src={movie.imageUrl} alt={movie.title} className="moviesDetailsPageImages" />
+                    <br></br>
+                    <button onClick={editMovie}>Edit Movie</button>
+                    <button onClick={deleteMovie}>Delete Movie</button>
+                </>
+            )}
         </div>
     );
 }
 
 export default MoviesDetailsPage;
-
-
-
